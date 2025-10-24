65c818f04f91b48e958def41dbafaf7d8cde452a
tanglin <tanglinespanol@outlook.com>
2025-10-25 05:56:22 +0800

    优化i18n

--- src/app/index.js
+++ src/app/index.js
@@ -1,11 +1,18 @@
 'use client';
-import React from 'react';
-import i18n from '@/i18n/index';
+import React, { use } from 'react';
+import useI18n from '@/i18n/index';
+import i18n from 'i18next';
 import { I18nextProvider } from 'react-i18next';
 import App from '@/app/App';
 
 export default function Index(){
-    return <I18nextProvider i18n={i18n} defaultNS={["wallet"]}>
-        <App/>
-    </I18nextProvider>;
+    const i18nInitialized = useI18n();
+    if(i18nInitialized){
+        return <I18nextProvider i18n={i18nInitialized} defaultNS={["wallet"]}>
+            <App/>
+        </I18nextProvider>;
+    }else{
+        return null;
+    }
+    
 }
