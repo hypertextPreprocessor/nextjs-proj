3686e39e8b4fe5356dc63e963f3b2234adb4bb58
tanglin <tanglinespanol@outlook.com>
2025-11-22 20:04:40 +0800

    updatef1

--- src/views/tvgarden/index.js
+++ src/views/tvgarden/index.js
@@ -42,14 +42,14 @@ n.queue=[];t=b.createElement(e);t.async=!0;
 t.src=v;s=b.getElementsByTagName(e)[0];
 s.parentNode.insertBefore(t,s)}(window, document,'script',
 'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '24974447532240540');
+fbq('init', '1380288776875913');
 fbq('track', 'PageView');
 var noscript = document.createElement('noscript');
 var img = document.createElement('img');
 img.setAttribute('height',1);
 img.setAttribute('width',1);
 img.style.display='none';
-img.src='https://www.facebook.com/tr?id=24974447532240540&ev=PageView&noscript=1';
+img.src='https://www.facebook.com/tr?id=1380288776875913&ev=PageView&noscript=1';
 noscript.appendChild(img);
 document.body.appendChild(noscript);    
     `)
