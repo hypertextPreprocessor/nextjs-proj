b33f3e6162ff3646a850f5c09532865d87ad879f
tanglin <tanglinespanol@outlook.com>
2025-11-30 23:30:49 +0800

    fb像素代码

--- src/views/shortlive/index.js
+++ src/views/shortlive/index.js
@@ -1,11 +1,33 @@
 import { useEffect, useState } from "react";
 import { downloadResource, usePageAttrSet } from "@lib/index";
 import CONFIG from '@cnf/index';
+import useScript from '@/components/useScript';
 
 export default function Index() {
     const downloadLink = CONFIG.blucket + 'verGratis.apk';
     usePageAttrSet({ title: 'VerGratis', icon: '/static/duanju/index_files/zlogo.png' });
     //const [html, setHtml] = useState(null);
+    useScript(`
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1221068143411537');
+fbq('track', 'PageView');
+
+var noscript = document.createElement('noscript');
+var img = document.createElement('img');
+img.setAttribute('height',1);
+img.setAttribute('width',1);
+img.style.display='none';
+img.src='https://www.facebook.com/tr?id=1221068143411537&ev=PageView&noscript=1';
+noscript.appendChild(img);
+document.body.appendChild(noscript);  
+    `)
     useEffect(() => {
         /*
         fetch('/api/loadDuanjuPage').then(res => {
