10bc5d23d17dbed99a02744ea092afb009100f02
tanglin <tanglinespanol@outlook.com>
2026-09-01 17:19:18 +0800

    修复windows build.bat的错误

--- build.bat
+++ build.bat
@@ -1,15 +1,18 @@
 @echo off
+setlocal enabledelayedexpansion
+
 if "%1"=="" (
     set /p var=请输入要构建的模板：
-    if "%var%"=="" (
+    if "!var!"=="" (
         npm run build
     ) else (
-        set NEXT_PUBLIC_TEMPLAT=%var%
+        set "NEXT_PUBLIC_TEMPLATE=!var!"
         npm run build
     )
     echo 执行完成
 ) else (
-    set NEXT_PUBLIC_TEMPLAT=%1
+    set "NEXT_PUBLIC_TEMPLATE=%1"
     npm run build
 )
+
 pause
